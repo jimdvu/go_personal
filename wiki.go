53ae4fd376e05a7d72ba03fd887a33fb053146f4
Jimmy <jimdvu@gmail.com>
2021-08-15 21:25:14 -0500

    add time and style welcome

--- wiki.go
+++ wiki.go
@@ -7,17 +7,20 @@ import (
   "html/template"
   "regexp"
   "errors"
+	"time"
 )
 
 type Page struct {
 	Title string
 	Body  []byte
+	Time string
 }
 
 var templates = template.Must(template.ParseFiles())
 
 
 func main() {
+	welcome := Welcome{time.Now().Format(time.Stamp)}
 
 	log.Fatal(http.ListenAndServe("", nil))
 }
